ffb586a93a5384cc8a863d5916f26ccf7e622b72
jakobpalmborg <jakob.palmborg@gmail.com>
2023-02-28 13:48:46 +0100

    css for index

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+const password = document.querySelector('#create-password');
+password.addEventListener("keyup", passwordStrength);
+
+let indicatorContainer = document.querySelector('#strength-indicator');
+let indicatorValue = document.createElement("small");
+
+
+function passwordStrength() {
+    if (password.value.length < 4) {
+        indicatorValue.innerHTML = "svagt";
+        indicatorContainer.appendChild(indicatorValue);
+    } else if (password.value.length < 7) {
+        indicatorValue.innerHTML = "medium";
+        indicatorContainer.appendChild(indicatorValue);
+    } else {
+        indicatorValue.innerHTML = "starkt";
+        indicatorContainer.appendChild(indicatorValue);
+    }
+}
